d4031c41b51cf39b268300acf5924d8b690b3b4c
Ahzam Khan <ahzam.main@gmail.com>
2023-03-19 16:19:47 +0530

    sequence as well

--- components/ngl.js
+++ components/ngl.js
@@ -1,22 +1,74 @@
 import React from "react";
 import { Stage, Component } from "react-ngl";
-import { useMemo } from "react";
-export default function s({path}) {
-  const reprList = useMemo(
-    () => [
-      {
-        type: "ball+stick",
-      },
-    ],
+import { useMemo, useState, useCallback } from "react";
+export default function s({ path }) {
+  const [reprName, setReprName] = useState("cartoon");
+  const reprLists = useMemo(
+    () => ({
+      "ball+stick": [
+        {
+          type: "ball+stick",
+        },
+      ],
+      cartoon: [
+        {
+          type: "cartoon",
+        },
+      ],
+      "ribbon and line": [
+        {
+          type: "ribbon",
+          param: {
+            color: "atomindex",
+          },
+        },
+        {
+          type: "line",
+          param: {
+            color: "element",
+          },
+        },
+      ],
+      spacefill: [
+        {
+          type: "spacefill",
+          param: {
+            color: "element",
+          },
+        },
+      ],
+      surface: [
+        {
+          type: "surface",
+          param: {
+            color: "element",
+          },
+        },
+      ],
+    }),
+    []
+  );
+  const handleReprChange = useCallback(
+    (event) => setReprName(event.target.value),
     []
   );
 
-  console.log(reprList);
   return (
     <>
       <Stage width="600px" height="400px">
-        <Component path={path} reprList={reprList} />
+        <Component path={path} reprList={reprLists[reprName]} />
       </Stage>
+      <select
+        name="representation"
+        value={reprName}
+        onChange={handleReprChange}
+      >
+        {Object.keys(reprLists).map((name) => (
+          <option key={name} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import { useState, useEffect } from "react";
+import ReactFileReader from "react-file-reader";
+
 const Structure = dynamic(
   () => {
     return import("../components/ngl");
@@ -9,15 +11,87 @@ const Structure = dynamic(
 );
 export default function index() {
   const [fileImg, setFileImg] = useState(null); // storing file data
-  useEffect(() => {
-    console.log(fileImg);
-  }, [fileImg]);
+
+  const [fileContent, setFileContent] = useState("");
+  const [fasta, setFasta] = useState(null); // storing file data
+
+  function handleFileRead(e) {
+    const content = e.target.result;
+    setFileContent(content);
+  }
+
+  function handleFileChosen(file) {
+    setFileImg(file);
+    const reader = new FileReader();
+    reader.onloadend = handleFileRead;
+    reader.readAsText(file);
+  }
+
+  const AminoAcids = {
+    ALA: "A",
+    ARG: "R",
+    ASN: "N",
+    ASP: "D",
+    CYS: "C",
+    GLU: "E",
+    GLN: "Q",
+    GLY: "G",
+    HIS: "H",
+    ILE: "I",
+    LEU: "L",
+    LYS: "K",
+    MET: "M",
+    PHE: "F",
+    PRO: "P",
+    SER: "S",
+    THR: "T",
+    TRP: "W",
+    TYR: "Y",
+    VAL: "V",
+  };
+
+  const pdbToFASTA = (pdbText) => {
+    let fastaText = "";
+    const lines = typeof pdbText === "string" ? pdbText.split("\n") : "";
+
+    let currentChain = null;
+
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      if (line.startsWith("ATOM")) {
+        const chain = line[21];
+        if (chain !== currentChain) {
+          fastaText += `>${chain}\n`;
+          currentChain = chain;
+        }
+        const resName = line.substr(17, 3).trim();
+        fastaText += AminoAcids[resName] || "X";
+      } else if (line.startsWith("TER")) {
+        currentChain = null;
+        fastaText += "\n";
+      }
+    }
+    return fastaText;
+  };
 
   return (
     <div>
       input a file <br />
-      <input type="file" onChange={(e) => setFileImg(e.target.files[0])} />
-      {fileImg == null ? "" : <Structure path={fileImg} />}
+      <input
+        id="myBtn"
+        type="file"
+        onChange={(e) => handleFileChosen(e.target.files[0])}
+        accept=".pdb"
+      />
+      {/* <button onClick={pdbToFASTA(fileImg)} /> */}
+      {fileImg == null ? (
+        ""
+      ) : (
+        <>
+          <Structure path={fileImg} />
+          <p>{pdbToFASTA(fileContent)}</p>
+        </>
+      )}
     </div>
   );
 }
